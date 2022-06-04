cbe484b254376c90fc8dacb4b4fcbd5d88968f6a
Mark Refaat <mark1711712@miuegypt.edu.eg>
2022-06-04 16:37:19 +0200

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :applications, param: :access_token do
+    resources :chats, param: :number do
+      resources :messages, param: :message_id
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
